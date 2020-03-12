488db9572249fe0af0a79ba01a9c5f4254e1927d
aashishb-access <aashish.b@access-designs.com>
2020-03-12 13:30:58 +0530

    without using git tag version

--- updateScript.js
+++ updateScript.js
@@ -1,8 +1,7 @@
 const fs = require('fs');
-const gitVersion = require('git-tag-version');
+const cp = require('child_process');
 
 const filePath = 'src/appVersion.js';
-
 
 fs.readFile(filePath, 'utf8', (err, data) => {
   if (err) {
@@ -10,7 +9,8 @@ fs.readFile(filePath, 'utf8', (err, data) => {
   }
 
   // get git tag
-  const tagVersion = gitVersion();
+  const lastTagCommand = 'git describe --abbrev=0 --tags';
+  const tagVersion = cp.execSync(lastTagCommand, { cwd: '.' }).toString().match(/[\d\.+]+/)[0];
   let result = data.replace(/appVersion\s?:\s?'\d{1,2}\.\d{1,2}\.\d{1,2}',/gm, `appVersion: '${tagVersion}',`);
   console.log(`Updated App version to  ${tagVersion}`);
 
@@ -18,9 +18,8 @@ fs.readFile(filePath, 'utf8', (err, data) => {
   const date = new Date();
   const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jul', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
   const buildDate = `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
-
   result = result.replace(/appBuildDate\s?:\s?'[a-zA-z]{3}\s\d{1,2},\s\d{4}'/gm, `appBuildDate: '${buildDate}'`);
-  console.log(`Updated build to  ${tagVersion}`);
+  console.log(`Updated build date to  ${buildDate}`);
 
   fs.writeFile(filePath, result, 'utf8', (writeErr) => {
     if (writeErr) return console.log(err);
@@ -28,5 +27,3 @@ fs.readFile(filePath, 'utf8', (err, data) => {
   });
   return 0;
 });
-
-console.log(gitVersion());
